f65b94c794dc8e5851003f5acab676f402f9f667
bwaklog <aditya.mh@outlook.com>
2024-11-02 15:55:58 +0530

    fix: reserved keywords in dict not being recognised

--- bash_lex.py
+++ bash_lex.py
@@ -1,13 +1,12 @@
 from ply.src.ply import lex
 
-reserved: dict[str, str] = {
-    "!": "NOT", # Pipelines
-    ":": "EXPANSION",
-    ".": "READ_FILE",
-
+reserved = {
+    # "!": "NOT",  # Pipelines
+    # ":": "EXPANSION",
+    # ".": "READ_FILE",
     # Conditional Constructs
-    "[[": "CONDITIONAL_CONST_OPEN",
-    "]]": "CONDITIONAL_CONST_CLOSE",
+    # "[[": "CONDITIONAL_CONST_OPEN",
+    # "]]": "CONDITIONAL_CONST_CLOSE",
     "if": "IF",
     "then": "THEN",
     "else": "ELSE",
@@ -16,21 +15,17 @@ reserved: dict[str, str] = {
     "select": "SELECT",
     "case": "CASE",
     "esac": "ESAC",
-
     # Command Grouping
     "{": "COMMAND_GROUPING_OPEN",
     "}": "COMMAND_GROUPING_CLOSE",
-
     # Looping Constructs
     "do": "DO",
     "done": "DONE",
     "while": "WHILE",
     "until": "UNTIL",
     "continue": "CONTINUE",
-
     # Shell functions
     "function": "FUNCTION",
-
     # Shell Commands
     "alias": "ALIAS",
     "bg": "BG",
@@ -44,17 +39,23 @@ reserved: dict[str, str] = {
     "dirs": "DIRS",
     "disown": "DISOWN",
     "echo": "ECHO",
-    "declare": "DECLARE",
-    "break": "BREAK",
     "exec": "EXEC",
     "enable": "ENABLE",
     "export": "EXPORT",
 }
 
 # Below are the list of tokens
-tokens: tuple[str] = (
+tokens = (
+    "IDENTIFIER",
+    "STRING",
+    "COMMENT",
     "LPAREN",
     "RPAREN",
+    "CONDITIONAL_CONST_OPEN",
+    "CONDITIONAL_CONST_CLOSE",
+    "NOT",
+    "EXPANSION",
+    "READ_FILE",
     "PLUS",
     "MINUS",
     "MULTIPLY",
@@ -74,25 +75,72 @@ tokens: tuple[str] = (
 
 t_LPAREN = r"\("
 t_RPAREN = r"\)"
+t_CONDITIONAL_CONST_OPEN = r"\[\["
+t_CONDITIONAL_CONST_CLOSE = r"\]\]"
+t_NOT = r"!"
+t_EXPANSION = r":"
+t_READ_FILE = r"\."
 t_EQUALS = r"="
 t_PLUS = r"\+"
 t_MINUS = r"\-"
 t_MULTIPLY = r"\*"
 t_DIVIDE = r"/"
 t_GREATER = r"-gt"
-t_LESSER  = r"-lt"
+t_LESSER = r"-lt"
 t_GREATEREQUAL = r"-ge"
 t_LESSEREQUAL = r"-le"
-t_NOTEQUAL = r'-ne'
+t_NOTEQUAL = r"-ne"
+
+
+def t_IDENTIFIER(t):
+    # NOTE: idts this refex identifier is correct
+    r"[a-zA-Z][a-zA-Z0-9\-]*"
+    t.type = reserved.get(t.value, "IDENTIFIER")
+    return t
+
 
 def t_NUMBER(t):
-    r'\d+'
+    r"\d+"
     t.value = int(t.value)
     return t
 
 
+def t_STRING(t):
+    r"(\"[^\"]*\"|\'[^\']*\')"
+    return t
+
+
+def t_COMMENT(t):
+    r"\#.*"
+    return t
+
+
 def t_error(t):
-    print(f"Illegal character at {t.value[0]!r}") 
+    # print(f"Illegal character at {t.value[0]!r}")
     t.lexer.skip(1)
 
+
 lexer = lex.lex()
+
+if __name__ == "__main__":
+    test_input = """
+#This is a comment
+echo "hi"
+echo 'hi'
+a = 10
+if [[ 10 -eq 10]]; then 
+    echo "hi"
+fi
+"""
+    lexer.input(test_input)
+    while True:
+        tok = lexer.token()
+        if not tok:
+            break
+        data = {
+            "type": tok.type,
+            "value": tok.value,
+            "lineno": tok.lineno,
+            "lexpos": tok.lexpos,
+        }
+        print(data)
